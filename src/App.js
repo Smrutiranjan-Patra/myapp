77f39e3c8d56f14ba3bbbf552c5affe7d996b6c2
Smrutiranjan Patra <76831153+Smrutiranjan-Patra@users.noreply.github.com>
2022-01-03 15:38:34 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
 import "./App.css";
+import React from "react";
+import { Form } from "./components/form";
 
 function App() {
   return (
     <div className="App">
-      <form>
-        <div>
-          <label for="title">Title&nbsp;:&nbsp;</label>
-          <input type="text" name="title" />
-        </div>
-        <div>
-          <label for="ingredients">Ingredients &nbsp;: &nbsp;</label>
-          <input type="text" name="ingredients" />
-        </div>
-
-        <div>
-          <label for="time">Time&nbsp;:&nbsp;</label>
-          <input type="number" name="time" />
-        </div>
-
-        <div>
-          <label for="image">Image&nbsp;:&nbsp;</label>
-          <input type="text" name="image" />
-        </div>
-
-        <div>
-          <input type="submit" />
-        </div>
-      </form>
+      <Form />
+      
     </div>
   );
 }
